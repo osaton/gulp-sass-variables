32f09a115e75d9e15a5d1618774c53c1fd67426b
osaton <o@sploosh.og>
2016-09-25 17:44:46 +0300

    Prepend variables to file

--- index.js
+++ index.js
@@ -6,17 +6,36 @@ let gutil = require('gulp-util'),
     PluginError = gutil.PluginError,
     through = require('through2');
 
-module.exports = function(settings) {
+let getVariablesBuffer = function(sassVariables, file) {
+  let str = '';
+  
+  for(let variable in sassVariables) {
+    str += variable + ': ' + JSON.stringify(sassVariables[variable]) + ';\n';
+  }
 
-  return through.obj(function (file, encoding, callback) {
+  return new Buffer(str, file);
+}
+
+module.exports = function(sassVariables) {
+
+  return through.obj(function (file, encoding, cb) {
 
     if(file.isNull()) {
-
-    } else {
-
+      return cb(null, file);
     }
 
-    callback(null, file);
+    if(file.isStream()) {
+      return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+    }
+
+    if(sassVariables && typeof sassVariables === 'object') {
+      let variablesBuffer = getVariablesBuffer(sassVariables, file);
+      file.contents = Buffer.concat([variablesBuffer, file.contents], variablesBuffer.length + file.contents.length);
+    } else {
+      return cb(new PluginError(PLUGIN_NAME, 'Variables object expected'));
+    }
+
+    return cb(null, file);
 
   });
 };
